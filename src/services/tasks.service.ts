676d0124d2baea2de0d924018b337a7e28082c6d
francagiovanna <francamariagiovanna4@gmail.com>
2026-05-18 06:58:22 -0300

    refactor(tasks): atualiza import do repositório de histórico

--- src/services/tasks.service.ts
+++ src/services/tasks.service.ts
@@ -1,12 +1,12 @@
 import { tasksRepository } from "../repositories/tasks.repository"
-import { taskHistoryRepository } from "../repositories/taskHistory.repository"
+import { historyRepository } from "../repositories/history.repository"
 import { EventType } from "../prisma/generated/prisma/client"
 import type { CreateTaskDTO } from "../repositories/tasks.repository"
 
 export const tasksService = {
   async create(data: CreateTaskDTO) {
     const task = await tasksRepository.create(data)
-    await taskHistoryRepository.create(task.id, EventType.CREATED)
+    await historyRepository.create(task.id, EventType.CREATED)
     return task
   }
 }
