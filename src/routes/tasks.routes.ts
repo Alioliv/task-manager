0a7eed384bdc294d6ccc205eb9605b8458eeef23
LwBane <emiliepessoa8@gmail.com>
2026-05-20 21:42:38 -0300

    feat: adiciona rotas patch para editar e concluir tarefa

--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express"
 import { tasksController } from "../controllers/tasks.controller"
-import { authenticate, authorize } from "../middlewares/auth.middleware"
+import { authenticate } from "../middlewares/auth.middleware"
 
 const router = Router({ mergeParams: true })
 
 router.post("/", authenticate, tasksController.create)
-router.get("/", authenticate, tasksController.findMany)
 router.post("/:id/assignees", authenticate, tasksController.addAssignees)
-router.get("/:id/history", authenticate, authorize("ADMIN", "MEMBER"), tasksController.getHistory)
+router.get("/", authenticate, tasksController.findMany)
+router.get("/:id/history", authenticate, tasksController.getHistory)
+router.patch("/:id", authenticate, tasksController.update)
+router.patch("/:id/complete", authenticate, tasksController.complete)
 
 export default router
